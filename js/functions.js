de96f757bcb8028de9bc0f6b8c5903df8bd4ee9c
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2023-02-09 11:02:35 +0000

    Adding new features (sound effects, animation, score)
    First of all, we have refactored the code to defragment it into pieces it helped us to organize the code.
    
    Then we animated the homepage, using the animage module designed ealier
    
    Next, we added a score for the user, she will now be able to know her level and focus accordingly
    
    Lastly, we added some sound effects for fun, the user will now be able to notice by ears if she is doing well or not. In addition there are many sound effect for either success and failure

---
+++ js/functions.js
@@ -0,0 +1,299 @@
+
+/**
+ * Récolte et renvoie la réponse de l'utilisateur depuis le DOM
+ * 
+ * @returns {String}
+ */
+function getUserAnswer() {
+  let userAnswer = "";
+  switch(currentQuestion.type) {
+    case 'qcm' : 
+      userAnswer = $('.answers .selected').html()
+      break;
+    case 'qcm multiple' : 
+      userAnswer = Array.from($('.answers .selected')).map(el => el.innerHTML);
+      break;
+    case 'unique-answer' :
+      userAnswer = document.querySelector('.answers input').value;
+      break;
+    case 'true-false':
+      userAnswer = $('.answers .selected').data('value');
+      break;
+    }
+  
+  return userAnswer;
+}
+
+
+/**
+ * 
+ * @param {String | Array | Boolean} userAnswer la réponse de l'utilisateur
+ * @returns Renvoie vers la bonne page de status (échec ou succès)
+ */
+function validate(userAnswer) {
+  let success = true;
+  console.log({
+    "Réponse correcte" : currentQuestion.answer, 
+    "Réponse utilisateur" : userAnswer
+  });
+  switch(currentQuestion.type) {
+    case 'unique-answer' :
+      if(userAnswer.trim() === ""){
+        alert("Vous devez saisir une réponse !");
+        return;
+      }
+      if((''+currentQuestion.answer).trim().toLowerCase() !== userAnswer.trim().toLowerCase()) {
+        success = false;
+      }
+      break;
+    case 'true-false' :
+      if(userAnswer === undefined) {
+        alert("Vous devez choisir une option !");
+        return;
+      }
+      if(currentQuestion.answer != userAnswer) {
+        success = false;
+      }
+      break;
+    case 'qcm' :
+      let indAnswer = currentQuestion.propositions.indexOf(userAnswer);
+      
+      if(indAnswer === -1) {
+        alert("Veuillez sélectionner votre réponse !");
+        return;
+      }
+      if(indAnswer != currentQuestion.answer) {
+        success = false;
+      }
+      break;
+    case 'qcm multiple' :
+      if(userAnswer.length === 0) {
+        alert("Veuillez sélectionner au moins une réponse !")
+        return;
+      }
+      if(currentQuestion.answers.length != userAnswer.length) {
+        success = false;
+      } else {
+        let correctAnswers = currentQuestion.answers.map(a => currentQuestion.propositions[a]);
+        success = correctAnswers.every(a => userAnswer.indexOf(a) !== -1);
+      }
+      break;
+  }
+  
+  console.log(success);
+  let userInfo = getUserInfo();
+  userInfo.success = parseInt(userInfo.success);
+  userInfo.total = parseInt(userInfo.total);
+  userInfo.total++;
+
+  playSound(success);
+
+  if(success) {
+    location.hash = "success";
+    let [answer, desc] = getStatus(success);
+
+    $('.page.success .description p').html(desc);
+    $('.page.success .answer p').html(answer);
+
+    // modification du score utilisateur
+    userInfo.success++;
+    setUserInfo(userInfo);
+  } else {
+    location.hash = "error";
+    let [answer, desc] = getStatus(success);
+
+    $('.page.error .description p').html(desc);
+    $('.page.error .answer p').html(answer);
+
+    // modification du score utilisateur
+    setUserInfo({ total: userInfo.total});
+  }
+  updateUserStatus();
+}
+
+/**
+ * Récupères des informations à propos de la question courante
+ * 
+ * @param {Boolean} succeed Si oui ou non la réponse à la question est correcte
+ * @returns Un tableau contenant la vraie réponse et la description à la question
+ */
+function getStatus(succeed) {
+  let answerText = "", descText = "";
+  switch(currentQuestion.type) {
+    case 'true-false' : 
+      if(succeed) {
+        answerText = "La réponse était bien <b>Vrai</b>";
+      } else {
+        answerText = "La bonne réponse était <b>Faux</b>"
+      }
+      break;
+    case 'qcm':
+      if(succeed) {
+        answerText = `La bonne réponse était bien : <b>${currentQuestion.propositions[currentQuestion.answer]}</b>`;
+      } else {
+        answerText = `Malheureusement la bonne réponse était : <b>${currentQuestion.propositions[currentQuestion.answer]}</b>`;
+      }
+      break;
+    case 'qcm multiple' :
+      // si c'est une seule réponse, on l'affiche
+      if (succeed) {
+        answerText = currentQuestion.answers.length === 1 ? 'La ' : "Les ";
+        answerText += "bonne" + (currentQuestion.answers.length === 1 ? '' : "s");
+        answerText += " réponse" + (currentQuestion.answers.length === 1 ? '' : "s");
+        answerText += " étai"  + (currentQuestion.answers.length === 1 ? 't' : "ent");
+        answerText += " bien : ";
+        answerText += currentQuestion.answers.map(a => `<li style="margin-left: 5px;"><i>${currentQuestion.propositions[a]}</i></li>`).join('');
+      } else {
+        answerText = "Malheureusement "
+        answerText += currentQuestion.answers.length == 1 ? 'la ' : "les ";
+        answerText += "bonne" + (currentQuestion.answers.length == 1 ? '' : "s");
+        answerText += " réponse" + (currentQuestion.answers.length == 1 ? '' : "s");
+        answerText += " étai"  + (currentQuestion.answers.length == 1 ? 't' : "ent : ");
+        answerText += currentQuestion.answers.map(a => `<li style="margin-left: 5px;"><i>${currentQuestion.propositions[a]}<i></li>`).join('');
+      }
+      break;
+      case 'unique-answer' : 
+        if(succeed) {
+          answerText = (Math.random() < 0.5 ? "Bien joué ! " : '') + `La bonne réponse était bien :  <b>${currentQuestion.answer}</b>`;
+        } else {
+          answerText = Math.random() < 0.5 
+            ? "Oh la la ! Vous auriez dû dire : <b>" + currentQuestion.answer + "</b>"
+            : `Malheureusement la bonne réponse était : <b>${currentQuestion.answer}</b>`;
+        }
+        break;
+  }
+  descText = currentQuestion.description;
+  return [answerText, descText];
+}
+
+
+/**
+ * Renvoie une question tirée au hasard dans le tableau des questions
+ * @returns {Object}
+ */
+function getQuestion() {
+  let ind = rand(0, dataQuestions.length - 1);
+  
+  let questions = (Math.random() < 0.2
+    ? dataQuestions.filter(q => !q.alreadyAsked)
+    : dataQuestions
+  );
+
+  return questions[ind];
+}
+
+/**
+ * Tire un nombre aléatoire entre deux extrêmes 
+ * 
+ * @param {Number} a Valeur minimale
+ * @param {Number} b Valeur maximale
+ * @returns Nombre tiré aléatoirement
+ */
+function rand(a, b) {
+  let random = Math.floor(Math.random() * ( b - a + 1)) + a;
+  return random;
+}
+
+/**
+ * Afficher une question tirée au hasard dans la room
+ * 
+ * @param {Object} question Question à afficher dans l'interface utilisateur
+ */
+function displayQuestion(question) {
+  console.log(question);
+  // selon le type de la question
+  let questionSection = $('.room .question');
+  questionSection.html(question.question);
+  
+  let answerSection = $('.room .answers');
+  let html = "";
+  switch(question.type) {
+    case 'true-false':
+      html += '<div class="true-false">';
+        html += '<div class="true" data-value="1">Vrai</div>';
+        html += '<div class="false" data-value="0">Faux</div>';
+      html += '</div>';
+      break;
+    case 'qcm': case 'qcm multiple':
+      // mélanger les propositions
+      let propositions = [...question.propositions];
+      for(let i = 0; i < question.propositions.length - 2; i++) {
+        let randIndice = rand(i+1, question.propositions.length - 1);
+        let tmp = propositions[i];
+        propositions[i] = propositions[randIndice];
+        propositions[randIndice] = tmp;
+      }
+      html += "<div>";
+        html += question.type === 'qcm' ? '' : '<span class="multiple-choix-label">Plusieurs choix sont possibles !</span>';
+        html += '<div class="' + question.type + '">';
+          for(prop of propositions) {
+            html += '<div>' + prop + '</div>'
+          }
+        html += '</div>';
+      html += '</div>';
+    break;
+    case 'unique-answer':
+      html += '<div class="unique-answer">';
+        html += '<input type="text" placeholder="Votre réponse ..."  />';
+      html += '</div>';
+    break;
+  }
+  answerSection.html(html);
+}
+
+/**
+ * Récupère les informations de l'utilisateur courant
+ * 
+ * @returns {Object} Les informations utilisateur
+ */
+function getUserInfo() {
+  let username = localStorage.getItem('username') || 'anonymous';
+  let success = localStorage.getItem('success') || 0;
+  let total = localStorage.getItem('total') || 0;
+
+  return {username, success, total};
+}
+
+/**
+ * Modifie les informations de l'utilisateur courant
+ * 
+ * @param {Object} infos Informations de l'utilisateur courant
+ */
+function setUserInfo(infos) {
+  let {username, success, total} = infos;
+
+  if(username !== undefined) localStorage.setItem('username', username);
+  if(total !== undefined)    localStorage.setItem('total', total);
+  if(success !== undefined)  localStorage.setItem('success', success);
+}
+
+/**
+ * Actualise les informations utilisateurs au niveau de la page d'accueil
+ * 
+ */
+function updateUserStatus() {
+  let {username, success, total} = getUserInfo() 
+  $('.home .content .score .username').text(username);
+  $('.home .content .score .level .total').text(total);
+  $('.home .content .score .level .success').text(success);
+  $('.home .content .score .score-value').text(parseInt(success) * 5 + ' pts');
+}
+
+/**
+ * Joue une tonalité différente selon si l'utilisateur a ou non gagné
+ * 
+ * @param {Boolean} status Etat de la partie (gagné ou perdu)
+ */
+function playSound(status) {
+  // Nom des effets
+  let sounds = {
+    success: document.querySelectorAll('audio.success'),
+    error: document.querySelectorAll('audio.error'),
+  };
+
+  if(status) {
+    sounds.success[rand(0,sounds.success.length)].play()
+  } else {
+    sounds.error[rand(0,sounds.error.length)].play()
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,25 +1,31 @@
 var currentQuestion = getQuestion();
 
+// Au chargement de l'application
 $(document).on("ready", function() {
+  updateUserStatus();
   displayQuestion(currentQuestion);
 });
 
+// Lors du click sur continuer (error, success)
 $('.success .new-question, .error .new-question').click(function(){
   currentQuestion = getQuestion();
   displayQuestion(currentQuestion);
 });
 
+// Evènement à binder à chaque changement de page
 $(document).on('pagechange', function() {  
   $(".answers .true-false > *").on('click', function (e) {
     $(this).parent().children().removeClass('selected');
     $(this).addClass('selected');
   });
   
+  // Lors de la sélection d'une propotion (qcm simple)
   $(".answers .qcm:not(.multiple) > *").on('click', function (e) {
     $(this).parent().children().removeClass('selected');
     $(this).addClass('selected');
   });
   
+  // Lors de la selection d'une proposition (qcm multiple)
   $(".answers .qcm.multiple > *").on('click', function (e) {
     if(!$(this).hasClass('selected')) {
       $(this).addClass('selected');
@@ -27,245 +33,31 @@ $(document).on('pagechange', function() {
       $(this).removeClass('selected');
     }
   }); 
-
-  // validation par appui sur la touche entrée lors de la saise des réponses (unique-answer)
-  $(".unique-answer").on('keyup', (e) => {
-    if(e.originalEvent.key === 'Enter') {
+  
+  // validation par appui sur la touche entrée lors de la saisie des réponses (unique-answer)
+  $('.unique-answer input').on('keyup', e => {
+    if(e.originalEvent.key == "Enter") {
       validate(getUserAnswer());
     }
   });
 });
 
-// évènement pour la validation de la question
-$('.actions .validate').click((e) => {
+// évènement pour la validation de la question (room)
+$('.room .actions .validate').click((e) => {
   e.preventDefault();
   validate(getUserAnswer());
-})
+});
 
-$('.unique-answer input').on('keyup', e => {
-  if(e.originalEvent.key == "Enter") {
-    validate(getUserAnswer());
+// Lors de l'appui du boutton valider (modification du nom d'utilisateur)
+$('.user .actions .validate').click((e)=>{
+  e.preventDefault();
+  let currentUsername= $('.user .user-info input')[0].value;
+  if(currentUsername == ""){
+    alert("Entrez votre pseudo svp!");
+    return ;
   }
-})
-
-/**
- * Récolte et renvoie la réponse de l'utilisateur depuis le DOM
- * 
- * @returns {String}
- */
-function getUserAnswer() {
-  let userAnswer = "";
-  switch(currentQuestion.type) {
-    case 'qcm' : 
-      userAnswer = $('.answers .selected').html()
-      break;
-    case 'qcm multiple' : 
-      userAnswer = Array.from($('.answers .selected')).map(el => el.innerHTML);
-      break;
-    case 'unique-answer' :
-      userAnswer = document.querySelector('.answers input').value;
-      break;
-    case 'true-false':
-      userAnswer = $('.answers .selected').data('value');
-      break;
-    }
-  
-  return userAnswer;
-}
-
-
-/**
- * 
- * @param {String | Array | Boolean} userAnswer la réponse de l'utilisateur
- * @returns Renvoie vers la bonne page de status (échec ou succès)
- */
-function validate(userAnswer) {
-  let success = true;
-  switch(currentQuestion.type) {
-    case 'unique-answer' :
-      if(userAnswer.trim() === ""){
-        alert("Vous devez saisir une réponse !");
-        return;
-      }
-      console.log(currentQuestion.answer, userAnswer)
-      if((''+currentQuestion.answer).trim().toLowerCase() !== userAnswer.trim().toLowerCase()) {
-        success = false;
-      }
-      break;
-    case 'true-false' :
-      if(userAnswer === undefined) {
-        alert("Vous devez choisir une option !");
-        return;
-      }
-      if(currentQuestion.answer != userAnswer) {
-        success = false;
-      }
-      break;
-    case 'qcm' :
-      let indAnswer = currentQuestion.propositions.indexOf(userAnswer);
-      
-      if(indAnswer === -1) {
-        alert("Veuillez sélectionner votre réponse !");
-        return;
-      }
-      if(indAnswer != currentQuestion.answer) {
-        success = false;
-      }
-      break;
-    case 'qcm multiple' :
-      if(userAnswer.length === 0) {
-        alert("Veuillez sélectionner au moins une réponse !")
-        return;
-      }
-      if(currentQuestion.answers.length != userAnswer.length) {
-        success = false;
-      } else {
-        let correctAnswers = currentQuestion.answers.map(a => currentQuestion.propositions[a]);
-        success = correctAnswers.every(a => userAnswer.indexOf(a) !== -1);
-      }
-      break;
-  }
-  
-  console.log(success);
-  if(success) {
-    location.hash = "success";
-    let [answer, desc] = getStatus(success);
-
-    $('.page.success .description p').html(desc);
-    $('.page.success .answer p').html(answer);
-
-  } else {
-    location.hash = "error";
-    let [answer, desc] = getStatus(success);
-
-    $('.page.error .description p').html(desc);
-    $('.page.error .answer p').html(answer);
-
-  }
-}
-
-
-/**
- * Récupères des informations à propos de la question courante
- * 
- * @param {Boolean} succeed Si oui ou non la réponse à la question est correcte
- * @returns Un tableau contenant la vraie réponse et la description à la question
- */
-function getStatus(succeed) {
-  let answerText = "", descText = "";
-  switch(currentQuestion.type) {
-    case 'true-false' : 
-      if(succeed) {
-        answerText = "La réponse était bien <b>Vrai</b>";
-      } else {
-        answerText = "La bonne réponse était <b>Faux</b>"
-      }
-      break;
-    case 'qcm':
-      if(succeed) {
-        answerText = `La bonne réponse était bien : <b>${currentQuestion.propositions[currentQuestion.answer]}</b>`;
-      } else {
-        answerText = `Malheureusement la bonne réponse était : <b>${currentQuestion.propositions[currentQuestion.answer]}</b>`;
-      }
-      break;
-    case 'qcm multiple' :
-      // si c'est une seule réponse, on l'affiche
-      if (succeed) {
-        answerText = currentQuestion.answers.length === 1 ? 'La ' : "Les ";
-        answerText += "bonne" + (currentQuestion.answers.length === 1 ? '' : "s");
-        answerText += " réponse" + (currentQuestion.answers.length === 1 ? '' : "s");
-        answerText += " étai"  + (currentQuestion.answers.length === 1 ? 't' : "ent");
-        answerText += " bien : ";
-        answerText += currentQuestion.answers.map(a => `<li style="margin-left: 5px;"><i>${currentQuestion.propositions[a]}</i></li>`).join('');
-      } else {
-        answerText = "Malheureusement "
-        answerText += currentQuestion.answers.length == 1 ? 'la ' : "les ";
-        answerText += "bonne" + (currentQuestion.answers.length == 1 ? '' : "s");
-        answerText += " réponse" + (currentQuestion.answers.length == 1 ? '' : "s");
-        answerText += " étai"  + (currentQuestion.answers.length == 1 ? 't' : "ent : ");
-        answerText += currentQuestion.answers.map(a => `<li style="margin-left: 5px;"><i>${currentQuestion.propositions[a]}<i></li>`).join('');
-      }
-      break;
-      case 'unique-answer' : 
-        if(succeed) {
-          answerText = (Math.random() < 0.5 ? "Bien joué ! " : '') + `La bonne réponse était bien :  <b>${currentQuestion.answer}</b>`;
-        } else {
-          answerText = Math.random() < 0.5 
-            ? "Oh la la ! Vous auriez dû dire : <b>" + currentQuestion.answer + "</b>"
-            : `Malheureusement la bonne réponse était : <b>${currentQuestion.answer}</b>`;
-        }
-        break;
-  }
-  descText = currentQuestion.description;
-  return [answerText, descText];
-}
-
-
-/**
- * Renvoie une question tirée au hasard dans le tableau des questions
- * @returns {Object}
- */
-var i = 0;
-function getQuestion() {
-  let ind = Math.floor(Math.random() * dataQuestions.length);
-  return dataQuestions[35];
-}
-
-/**
- * Tire un nombre aléatoire entre deux extrêmes 
- * 
- * @param {Number} a Valeur minimale
- * @param {Number} b Valeur maximale
- * @returns Nombre tiré aléatoirement
- */
-function rand(a, b) {
-  let random = Math.floor(Math.random() * ( b - a + 1)) + a;
-  return random;
-}
-
-/**
- * Afficher une question tirée au hasard dans la room
- * 
- * @param {Object} question Question à afficher dans l'interface utilisateur
- */
-function displayQuestion(question) {
-  console.log(question);
-  // selon le type de la question
-  let questionSection = $('.room .question');
-  questionSection.html(question.question);
-  
-  let answerSection = $('.room .answers');
-  let html = "";
-  switch(question.type) {
-    case 'true-false':
-      html += '<div class="true-false">';
-        html += '<div class="true" data-value="1">Vrai</div>';
-        html += '<div class="false" data-value="0">Faux</div>';
-      html += '</div>';
-      break;
-    case 'qcm': case 'qcm multiple':
-      // mélanger les propositions
-      let propositions = [...question.propositions];
-      for(let i = 0; i < question.propositions.length - 2; i++) {
-        let randIndice = rand(i+1, question.propositions.length - 1);
-        let tmp = propositions[i];
-        propositions[i] = propositions[randIndice];
-        propositions[randIndice] = tmp;
-      }
-      html += "<div>";
-        html += question.type === 'qcm' ? '' : '<span class="multiple-choix-label">Plusieurs choix sont possibles !</span>';
-        html += '<div class="' + question.type + '">';
-          for(prop of propositions) {
-            html += '<div>' + prop + '</div>'
-          }
-        html += '</div>';
-      html += '</div>';
-    break;
-    case 'unique-answer':
-      html += '<div class="unique-answer">';
-        html += '<input type="text" placeholder="Votre réponse ..."  />';
-      html += '</div>';
-    break;
-  }
-  answerSection.html(html);
-}
+  $('.user .user-info input')[0].value="";
+  setUserInfo ({username: currentUsername.trim().slice(0,10)});
+  updateUserStatus();
+  location.hash='home';
+});
